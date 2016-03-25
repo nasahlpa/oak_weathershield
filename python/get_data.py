251418c208f37db6c9540fb93956b80c816b2eed
Pascal Nasahl <pascal.nasahl@student.tugraz.at>
2016-03-25 17:36:31 +0100

    added: initial python file

--- python/get_data.py
+++ python/get_data.py
@@ -1,12 +1,13 @@
-import urllib
+from sseclient import SSEClient
 import json
+import re
 import MySQLdb
 import time
-DB_USER = "TYPE_USERNAME"
-DB_PW = "TYPE_PW"
-DB_NAME = "TYPE_DB"
-TOKEN = "TYPE_TOKEN"
-DEVICE = "TYPE_DEVICE"
+DB_USER = "USERNAME"
+DB_PW = "PASSWORD"
+DB_NAME = "weather"
+TOKEN = "DEVICE TOKEN"
+DEVICE = "DEVICE ID"
 
 def writeDB(date, temperature, ambient, pressure, humidity):
   db = MySQLdb.connect(host="localhost", user=DB_USER, passwd=DB_PW, db=DB_NAME)
@@ -14,24 +15,22 @@ def writeDB(date, temperature, ambient, pressure, humidity):
   cursor.execute("INSERT INTO weather (date, temperature, ambient, pressure, humidity) VALUES ('"+date+"', '"+str(temperature)+"', '"+str(ambient)+"', '"+str(pressure)+"', '"+str(humidity)+"');")
   db.commit()
   db.close()
-  
-def fetchURL(identifier):
-  base = "https://api.particle.io/v1/devices/"
-  var = "/"+identifier;
-  option = "?access_token=" 
-  
-  url = base + DEVICE + var + option + TOKEN
-  data = urllib.urlopen(url).read()
-  data_json = json.loads(data)
-  
-  return round(float(data_json["result"]), 2)
-  
-def getData():
-  now = time.strftime('%Y-%m-%d %H:%M:%S')
-  temp = fetchURL("temperature")
-  amb = fetchURL("ambient")
-  press = fetchURL("pressure")
-  hum = fetchURL("humidity")
-  writeDB(now, temp, amb, press, hum)
 
-getData()
+deviceID = 'd957040111349e5e9de61914'
+accessToken = 'e61fb85a90a4b5ed7b0e99979133f1ca8a9ede93'
+sparkURL = 'https://api.particle.io/v1/devices/' + deviceID + '/events/?access_token=' + accessToken
+
+messages = SSEClient(sparkURL)
+parse = re.compile('weather;(.*?)","ttl"')
+
+for msg in messages:
+  data = parse.search(msg.data)
+  if data:
+    data_split = data.group(1).split(';')
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
+    temperature = data_split[0]
+    ambient = data_split[1]
+    pressure = data_split[2]
+    humidity = data_split[3]
+    writeDB(now, temperature, ambient, pressure, humidity)
+    
